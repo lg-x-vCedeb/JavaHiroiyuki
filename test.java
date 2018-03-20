eb2e15d0e5cef9af2741685af5dedf2588ac81b0
lg_x_vCedeb <lg-x-vCedeb@users.noreply.github.com>
2018-03-20 11:53:01 +0800

    Create test.java

---
+++ test.java
@@ -0,0 +1,13 @@
+public class Student {
+	private int ID;
+	public Student(int ID) {
+		this.ID = ID;
+	}
+	public int getID() {
+		
+	}
+	public static void TestStudent() {
+		Student S = new Student(ID);
+		System.out.println(S.getID() == )
+	}
+}
