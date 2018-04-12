ef8b6c16dda2a7e9c810e868d13858ddb457820d
lg_x_vCedeb <lg-x-vCedeb@users.noreply.github.com>
2018-04-12 17:47:39 +0800

    Update Ass3.java

--- Object-Oriented-Programming/Ass3.java
+++ Object-Oriented-Programming/Ass3.java
@@ -53,7 +53,9 @@ class Chicken extends Bird{
 		super(color,numOfFeathers);
 	}
 	
-	public Chicken chick1 = new Chicken(getColor(), 8000);
+	public Chicken(String color){
+		super(color,8000);
+	}
 	
 	public String getColor(){
 		return color;
@@ -121,21 +123,25 @@ class Animal{
 	}
 	
 	public boolean isEdible(){
-		try {
-			return isEdible();//The subclass override the isEdible();
-		}
-		catch{
-			throw new Exception("We need to know what kind of animal it is or they are");
-		}
-		finally {
-			System.out.println("Do not eat this!");
-			return false;
-		}
+		System.out.println("We need to know what kind of animal it is or they are.");
+		return false;
 	}
 	
 	public static void testAnimal(){
 		Animal a1 = new Animal("Green");
 		System.out.println("The " + a1.getClass().getName() + "'s color is " + a1.getColor() + ", which is " + a1.getColor().equals("Green"));
 		System.out.println(a1.isEdible());
+		
+		Animal b1 = new Bird("Blue",314);
+		System.out.println("The " + b1.getClass().getName() + "'s color is " + b1.getColor() + ", which is " + b1.getColor().equals("Blue"));
+		System.out.println(b1.isEdible());
+		
+		Animal c1 = new Chicken("Yellow",340);
+		System.out.println("The " + c1.getClass().getName() + "'s color is " + c1.getColor() + ", which is " + c1.getColor().equals("Yellow"));
+		System.out.println(c1.isEdible());
+		
+		Animal c2 = new Chicken("Yellow");
+		System.out.println("The " + c2.getClass().getName() + "'s color is " + c2.getColor() + ", which is " + c2.getColor().equals("Green"));
+		System.out.println(c2.isEdible());
 	}
 }
