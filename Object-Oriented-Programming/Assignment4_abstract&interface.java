21a64cdf12f6fcfcec226671badb63ee7dc751e8
lg_x_vCedeb <lg-x-vCedeb@users.noreply.github.com>
2018-04-19 11:44:14 +0800

    Update Assignment4_abstract&interface.java

--- Object-Oriented-Programming/Assignment4_abstract&interface.java
+++ Object-Oriented-Programming/Assignment4_abstract&interface.java
@@ -2,14 +2,15 @@
 public class Start {
 	public static void main(String[] args) {
 		Jellyfish.testJellyfish();
+		Fish.testFish();
 	}
 }
 
 class Fish implements Swimmer{
 	private String name;
-	private Fish friend;
+	private Swimmer friend;
 	
-	public Fish(String name, Fish friend) {
+	public Fish(String name, Swimmer friend) {
 		this.name = name;
 		this.friend = friend;
 	}
@@ -18,21 +19,21 @@ class Fish implements Swimmer{
 		return name;
 	}
 	
-	public Fish getFriend() {
+	public Swimmer getFriend() {
 		return friend;
 	}
 	
 	public static void testFish() {
 		Swimmer j1 = new Jellyfish("j1");
-		Swimmer f1 = new Fish("f1",(Fish) j1.getFriend());
-		Fish f2 = new Fish("f2",(Fish) f1);
+		Swimmer f1 = new Fish("f1",j1);
+		Fish f2 = new Fish("f2",f1);
 		Fish f3 = new Fish("f3",f2);
 		
 		System.out.println(j1.getName() == "j1");
 		System.out.println(j1.getFriend() == j1);
 		
 		System.out.println(f1.getName() == "f1");
-		System.out.println(f1.getFriend() == j1.getFriend());
+		System.out.println(f1.getFriend() == j1);
 		
 		System.out.println(f2.getName() == "f2");
 		System.out.println(f2.getFriend() == f1);
